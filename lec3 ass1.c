0593a9783fdcc8938e7cc448988abf297ce0ee88
Ismail Zakariea <smsmy.e12@gmail.com>
2026-07-08 16:03:48 +0300

    lec3 Assignment 1

---
+++ lec3 ass1.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n1=0,n2=0;
+    while (1){
+        printf("Enter First Number : ");
+        scanf("%d",&n1);
+        printf("\nEnter Secnond Number : ");
+        scanf("%d",&n2);
+        printf("\nSum = %d\n",n1 + n2);
+        
+    }
+   
+
+    return 0;
+}
