7dde86d679743a0806642ccaf1bfdb75db027d91
Ismail Zakariea <smsmy.e12@gmail.com>
2026-07-08 15:43:42 +0300

    Add second assignment

---
+++ lec1 ass2.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int b = 0 ;
+    int h = 0 ;
+    printf("Enter base ");
+    scanf("%d",&b);
+    printf("\nEnter hight ");
+    scanf("%d",&h);
+    printf("\nArea is : %d", b * h);
+   
+
+    return 0;
+}
