1046b65f24d9a8c650a426acffe4954a4be52130
Ismail Zakariea <smsmy.e12@gmail.com>
2026-07-08 15:23:18 +0300

    add first assginment

---
+++ lec1 ass1.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("I am semo");
+    printf("\n 9 10 20");
+    printf("\n %d %d %d",9,10,20);
+    // I can not understand the needed 
+
+    return 0;
+}
